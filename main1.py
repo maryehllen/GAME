f85780cba5aac962fe11cfbd4593597955566534
mary <maryehllen.rosa@escola.pr.gov.br>
2025-09-09 12:31:41 -0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,35 @@
+import pygame
+import os
+
+pygame.init()
+
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Janela com Imagem")
+
+BG_COLOR = (30, 30, 40)
+
+image_file = "moranguinho.png"
+if os.path.exists(image_file):
+    img = pygame.image.load(image_file).convert_alpha()
+    img_rect = img.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+else:
+    print("Imagem não encontrada")
+
+SPEED = 1
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+        
+    screen.fl(BG_COLOR)
+
+    screen.blit(img, img_rect.topleft)
+
+    pygame.display.flip()
+
+pygame.quit()
