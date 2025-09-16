f02e44f6df959fbd56be7d6a562a7468d05ac533
mary <maryehllen.rosa@escola.pr.gov.br>
2025-09-16 12:25:39 -0300

    Update main1.py

--- main1.py
+++ main1.py
@@ -2,26 +2,19 @@ import pygame
 
 
 pygame.init()
-
-
 WIDTH, HEIGHT = 1000, 250
-
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
 pygame.display.set_caption("Janela Simples")
 
 
 running = True
-
 while running:
-
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT:
-
             running = False
 
     pygame.display.flip()
 
 
 pygame.quit()
+
