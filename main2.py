0c850ec6249a355e9b0a222ec36e11c6db7a7522
mary <maryehllen.rosa@escola.pr.gov.br>
2025-09-16 12:27:47 -0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,74 +1,30 @@
 import pygame
-
 import os
-
-
-
-# Inici
 
 pygame.init()
 
-
-
-# Definindo o tamanho da janela
-
 WIDTH, HEIGHT = 1000, 720
-
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
 pygame.display.set_caption("Janela com Imagem")
 
+BG_COLOR = (30, 30, 40)
 
-
-# Definindo a cor de fundo
-
-BG_COLOR = (30, 30, 40)  # cor de fundo (um tom escuro)
-
-
-
-# Carregar a imagem
-
-image_file = "GAME\player.png"  # coloque o nome da sua imagem aqui
-
+image_file = "GAME\player.png"
 if os.path.exists(image_file):
-
-    img = pygame.image.load(image_file).convert_alpha()  # Carregar a imagem
-
-    img_rect = img.get_rect(center=(WIDTH // 2, HEIGHT // 2))  # Centraliza a imagem
+    img = pygame.image.load(image_file).convert_alpha()
+    img_rect = img.get_rect(center=(WIDTH // 2, HEIGHT // 2))
 
 else:
-
     print("Imagem não encontrada!")
-
-
-
-# Loop principal do jogo
-
 running = True
-
 while running:
-
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT:
-
             running = False
-
-
-
-    # Preencher o fundo
 
     screen.fill(BG_COLOR)
 
-
-
-    # Desenhar a imagem na tela
-
     screen.blit(img, img_rect.topleft)
-
-
-
-    # Atualizar a tela
 
     pygame.display.flip()
 
@@ -77,3 +33,4 @@ while running:
 # Finalizar o Pygame
 
 pygame.quit()
+
